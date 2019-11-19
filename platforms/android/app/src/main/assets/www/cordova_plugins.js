4a7e379ea6150656ddf735fd8bfd0c5db9a98691
Zach Baynash <zach@zachbaynash.com>
2019-11-19 15:39:39 +1100

    did everything

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,13 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-whitelist.whitelist",
+      "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+      "pluginId": "cordova-plugin-whitelist",
+      "runs": true
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.0.0"
+  };
+});
